5008aa5686350f9ab3a686345575a90943639ec7
ABIbreak <158116564+ABIbreak@users.noreply.github.com>
2026-02-12 20:01:15 -0600

    Negate reset, add switches to control convolution mode

--- v/convolution.sv
+++ v/convolution.sv
@@ -5,19 +5,33 @@ module convolution #(
 )(
     input  logic i_clk,
     input  logic i_rst_n,
+    input  logic i_horizontal,
 
     /* Convolution stuff */
     input  logic i_val_valid,
     input  logic [DATA_WIDTH-1:0] i_val,
     output logic o_val_valid,
-    output logic [DATA_WIDTH-1+3:0] o_val
+    output logic [17:0] o_val
 );
 
-    localparam int KERNEL[3][3] = '{
-        '{-4, 0, 4},
-        '{-8, 0, 8},
-        '{-4, 0, 4}
-    };
+    logic signed [2:0] KERNEL[N][N];
+
+    always_comb begin
+        if (i_horizontal) begin
+            KERNEL = '{
+                '{-1, 0, 1},
+                '{-2, 0, 2},
+                '{-1, 0, 1}
+            };
+        end
+        else begin
+            KERNEL = '{
+                '{ -1, -2, -1},
+                '{  0,  0,  0},
+                '{  1,  2,  1}
+            };
+        end
+    end
 
     typedef struct packed {
         logic valid;
@@ -47,9 +61,15 @@ module convolution #(
 
     /* Shift each row down one */
     always_ff @(posedge i_clk, negedge i_rst_n) begin
-        if (!i_rst_n) begin
-            for (int i = 0; i < 3; i++) begin
-                for (int j = 0; j < 3; j++) begin
+        for (int i = 0; i < 3; i++) begin
+            if (!i_rst_n) begin
+                _internal_grid[i][0] <= '0;
+            end
+            else begin
+                _internal_grid[i][0] <= _internal_fifo_out[i];
+            end
+            for (int j = 1; j < 3; j++) begin
+                if (!i_rst_n) begin
                     _internal_grid[i][j] <= '0;
                 end
             end
@@ -66,19 +86,31 @@ module convolution #(
         end
     end
 
-    logic [DATA_WIDTH-1+3:0] val;
+    value_t input_value;
+    assign input_value.valid = i_val_valid;
+    assign input_value.value = i_val;
+    
+    genvar i;
+    generate
+        for (i = 0; i < N; i++) begin : gen_line_buffer
+            Line_Buffer2 line_inst (
+                .clken(i_val_valid),
+                .clock(i_clk),
+                .shiftin((i == 0) ? input_value : _internal_grid[i-1][N-1]),
+                .taps1x(_internal_fifo_out[i])
+            );
+        end
+    endgenerate
+
+    logic signed [17:0] val;
     always_comb begin
         /* Perform convolution */
-        val = 
-            KERNEL[0][0] * $signed(_internal_grid[0][0].value) +
-            KERNEL[0][1] * $signed(_internal_grid[0][1].value) +
-            KERNEL[0][2] * $signed(_internal_grid[0][2].value) +
-            KERNEL[1][0] * $signed(_internal_grid[1][0].value) +
-            KERNEL[1][1] * $signed(_internal_grid[1][1].value) +
-            KERNEL[1][2] * $signed(_internal_grid[1][2].value) +
-            KERNEL[2][0] * $signed(_internal_grid[2][0].value) +
-            KERNEL[2][1] * $signed(_internal_grid[2][1].value) +
-            KERNEL[2][2] * $signed(_internal_grid[2][2].value);
+        val = 0;
+        for (int i = 0; i < N; i++) begin
+            for (int j = 0; j < N; j++) begin
+                val += $signed(_internal_grid[i][j].value) * KERNEL[i][j];
+            end
+        end
     end
     
     always_ff @(posedge i_clk) begin
--- v/convolution_wrapper.sv
+++ v/convolution_wrapper.sv
@@ -4,10 +4,15 @@ module convolution_wrapper(
     input  logic i_clk,
     input  logic i_rst_n,
 
+    input  logic i_greyscale,
+    input  logic i_horizontal,
+
     input  logic [11:0] i_red,
     input  logic [11:0] i_green,
     input  logic [11:0] i_blue,
     input  logic i_valid,
+
+    input  logic [5:0] i_shift_amt,
 
     output logic [11:0] o_red,
     output logic [11:0] o_green,
@@ -18,20 +23,26 @@ module convolution_wrapper(
     logic [11:0] value;
     assign value = (i_blue * 2 + i_green * 3 + i_red * 3)/8;
 
-    logic [14:0] convolution_value;
+    logic [17:0] convolution_value;
+    logic convolution_valid;
     convolution convolution_inst (
         .i_clk,
         .i_rst_n,
 
+        .i_horizontal(i_horizontal),
+
         .i_val_valid(i_valid),
         .i_val(value),
 
-        .o_val_valid(o_valid),
+        .o_val_valid(convolution_valid),
         .o_val(convolution_value)
     );
 
-    assign o_red = convolution_value[14:3];
-    assign o_green = convolution_value[14:3];
-    assign o_blue = convolution_value[14:3];
+    logic [11:0] o_val;
+    assign o_val = (i_greyscale) ? value : convolution_value[i_shift_amt+:11];
+    assign o_valid = (i_greyscale) ? i_valid : convolution_valid;
+    assign o_red = o_val;
+    assign o_green = o_val;
+    assign o_blue = o_val;
 
 endmodule
